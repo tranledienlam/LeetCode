454d58e7893a9fda920deadf05b3571c590a8833
tranledienlam <lam.tranledien.com>
2023-05-15 09:34:04 +0700

    ex1-js

--- 1-TwoSum-easy.js
+++ 1-TwoSum-easy.js
@@ -37,12 +37,22 @@
 
 var twoSum = function (nums, target) {
     len = nums.length
-
-    for (i = 0; i < len; i++) {
+    result  = []
+    if(len > 1) {
+        for (i = 0; i < len; i++) {
+            for (j = i+1; j < len;j++) {
+                if(nums[i]+nums[j]==target) {
+                    return result = [i,j]
+                }
+            }
+        }
     }
-};
-
+}
 // input
 
-var nums = [2,5,5,1,8,6]
-twoSum(nums,10)
+var nums = [3,3]
+console.log(twoSum(nums,6))
+var nums = [2,7,11,15]
+console.log(twoSum(nums,9))
+var nums = [3,2,4]
+console.log(twoSum(nums,6))
